d76b470f0e1ab45ebf5215f75c1d698c75066356
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2016-01-20 15:38:06 +0200

    Adding base layout

---
+++ Source/Web/FunnyDevs.Web/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace FunnyDevs.Web.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Source/Web/FunnyDevs.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FunnyDevs.Web.Startup))]
+namespace FunnyDevs.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
