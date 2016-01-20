4360a4d96fd18f9ba004d9ac6efda6d586731780
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2016-01-20 18:42:52 +0200

    Architecture almost finished (only need to add Ninject or Autofac)

---
+++ Source/FunnyDevs.Data/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using FunnyDevs.Data.Migrations;
+using FunnyDevs.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace FunnyDevs.Data
+{
+    public class DbContext : IdentityDbContext<User>, IDbContext
+    {
+        public DbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DbContext, Configuration>());
+        }
+
+        public static DbContext Create()
+        {
+            return new DbContext();
+        }
+    }
+}
